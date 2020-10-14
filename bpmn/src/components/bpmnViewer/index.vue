c1747af171187beb8ca8024360c76efb7f7008aa
haolsh <haolsh@dr.com>
2020-10-14 11:04:09 +0800

    调整代码结构

--- bpmn/src/components/bpmnViewer/index.vue
+++
@@ -1,9 +0,0 @@
-<script lang="jsx">
-    export default {
-        render() {
-            return <div>aaaaaa</div>
-        }
-    }
-</script>
-<style scoped>
-</style>
---
+++ process/babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    presets: [
+        'vue-cli-plugin-dr/preset'
+    ]
+}
